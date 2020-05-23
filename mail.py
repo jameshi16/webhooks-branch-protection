45505f73935746ed93bcec03d233d7d5c31eb20e
James <lim.en.hui.james@gmail.com>
2020-05-23 20:16:37 +0800

    Fix SMTP timeout bug

--- mail.py
+++ mail.py
@@ -1,4 +1,5 @@
 import smtplib
+from smtplib import SMTPSenderRefused
 from email.message import EmailMessage
 
 class Email:
@@ -6,23 +7,39 @@ class Email:
 	Class so that one SMTP connection is maintained for all emails
 	"""
 	def __init__(self, host, port, username, password, *, smtp_tls=False, smtp_ssl=False):
-		self.username = username
+		self.__host = host
+		self.__port = port
+		self.__username = username
+		self.__password = password
+		self.__smtp_tls = smtp_tls
+		self.__smtp_ssl = smtp_ssl
 
-		smtp_server = smtplib.SMTP(host, port=port) if not smtp_ssl else smtplib.SMTP_SSL(host, port=port)
-		if smtp_tls:
+		self.connect()
+
+	def connect(self):
+		smtp_server = smtplib.SMTP(self.__host, port=self.__port) if not self.__smtp_ssl else smtplib.SMTP_SSL(self.__host, port=self.__port)
+		if self.__smtp_tls:
 			smtp_server.starttls()
-		smtp_server.login(username, password)
-		self.smtp = smtp_server
+		smtp_server.login(self.__username, self.__password)
+		self.smtp = smtp_server	
 
 	def send_notification(self, email_addresses, subject, message):
 		msg = EmailMessage()
 		msg['Subject'] = subject
-		msg['From'] = self.username
+		msg['From'] = self.__username
 		msg['To'] = ', '.join(email_addresses)
-		msg.set_content(message)	
+		msg.set_content(message)
 
-		print("Email: Sent notification to email addresses")
-		self.smtp.send_message(msg)
+		try:
+			self.smtp.send_message(msg)
+			print("Email: Sent notification to email addresses")
+		except SMTPSenderRefused as e:
+			if e.smtp_code == 451:
+				print("Email: Disconnected from SMTP server. Reconnecting...")
+				self.connect()
+				self.send_notification(email_addresses, subject, message)
+			else:
+				raise e
 
 class FakeEmail:
 	"""
